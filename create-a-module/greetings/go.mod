9d6a96ea4aa706da3948d184833145c5a924f1b3
Trent V <trenton.vanderwert@gmail.com>
2023-09-04 14:49:08 -0500

    added gitignore and created a greetings.go

---
+++ create-a-module/greetings/go.mod
@@ -0,0 +1,3 @@
+module github.com/vtrenton/go-website-tutorials/create-a-module/greetings
+
+go 1.21.0
---
+++ create-a-module/greetings/greetings.go
@@ -0,0 +1,12 @@
+package greetings
+
+import "fmt"
+
+// Hello returns a greeting for the named person
+// note that the func name starts with a capitol letter
+// this allows it to be accessed outside of the current package
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
+}
